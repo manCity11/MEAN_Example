8331e2b4645f6053254ac3fa8cf6c3615d30c18c
Inthalak <inthalak.sirasa@gmail.com>
2016-12-23 21:59:24 +0100

    Beginning of the REST api

--- app.js
+++ app.js
@@ -10,6 +10,7 @@ var config = require('./config/database');
 mongoose.connect(config.database);
 
 var index = require('./routes/index');
+var api = require('./routes/api');
 var users = require('./routes/users');
 
 var app = express();
@@ -27,6 +28,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
+app.use('/api', api);
 app.use('/users', users);
 
 // catch 404 and forward to error handler
---
+++ routes/api.js
@@ -0,0 +1,5 @@
+var express = require('express');
+var jwt = require('jsonwebtoken');
+var router = express.Router();
+
+module.exports = router;
