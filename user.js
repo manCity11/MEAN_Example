721a69fb1e3d1f86bd51c6a377044c41ecfb6e80
Inthalak <inthalak.sirasa@gmail.com>
2016-12-23 21:46:04 +0100

    Create User model

---
+++ user.js
@@ -0,0 +1,39 @@
+var mongoose = require('mongoose');
+var bcrypt = require('brcypt');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+	username: {
+		type: String,
+		unique: true,
+		required: true
+	},
+	password: {
+		type: String,
+		required: true
+	},
+	admin: Boolean
+});
+
+userSchema.pre('save', function(next){
+	var that = this;
+	
+	bcrypt.genSalt(10, function(err, salt){
+		if(err){
+			throw err;
+		}
+		else{
+			bcrypt.hash(that.apssword, salt, function(err, hash){
+				if(err){
+					throw err;
+				}
+				else{
+					that.password = hash;
+					next();
+				}
+			});
+		}
+	});
+});
+
+module.exports = mongoose.model('Users', userSchema);
