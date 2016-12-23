27bda8d75b34642009d377bf3fcccb93fe1c1285
Inthalak <inthalak.sirasa@gmail.com>
2016-12-23 21:54:27 +0100

    Setup Database config

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var config = require('./config/database');
+
+mongoose.connect(config.database);
 
 var index = require('./routes/index');
 var users = require('./routes/users');
---
+++ config/database.js
@@ -0,0 +1,4 @@
+module.exports = {
+	'secret': 'mySecretPassword',
+	'database': 'mongodb://localhost/test'
+};
