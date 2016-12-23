db40103943b73062ade7e75874e0699c4535c236
Inthalak <inthalak.sirasa@gmail.com>
2016-12-23 22:34:11 +0100

    Authentication with web token implemented

---
+++ controllers/authenticateCtrl.js
@@ -0,0 +1,42 @@
+var jwt = require('jsonwebtoken');
+var bcrypt = require('bcrypt');
+var config = require('../config/database');
+
+var User = require('../models/user');
+
+module.exports.authenticate = function(req, res){
+	User.findOne({
+		username: req.body.username
+	}, function(err, user){
+		if(err){
+			res.status(500).json({
+				message: 'Impossible to find user'
+			});
+		}
+		if(!user){
+			res.status(500).json({
+				success: false,
+				message: 'Authentication failed. User not found'
+			});
+		}
+		else if(user){
+			if(bcrypt.compare(!req/body.password, user.password, function(err){})){
+				res.status(500).json({
+					success: false,
+					message: 'Authentication failed. Wrong password'
+				});
+			}
+			else{
+				var token = jwt.sign(user, config.secret, {
+					expiresIn: 4000
+				});
+
+				res.status(200).json({
+					success: true,
+					message: 'Authenticate successfully',
+					token: token
+				});
+			}
+		}
+	});
+}
--- routes/api.js
+++ routes/api.js
@@ -3,9 +3,34 @@ var jwt = require('jsonwebtoken');
 var router = express.Router();
 
 //Controllers
+var authenticateCtrl = require('../controllers/authenticateCtrl');
 var userCtrl = require('../controllers/userCtrl');
 
 router.post('/user/create', userCtrl.userCreate);
+
+//Middleware
+router.use(function(req, res, next){
+	var token = req.body.token || req.headers['token'];
+
+	if(token){
+		jwt.verify(token, config.secret, function(err, decode){
+			if(err){
+				res.status(500).json({
+					message: 'Invalid token'
+				});
+			}
+			else{
+				next();
+			}
+		});
+	}
+	else{
+		res.status(500).json({
+			message: 'Please send a token'
+		});
+	}
+});
+
 router.delete('/user/:username', userCtrl.userDelete);
 
 module.exports = router;
